503faa27abbfa8802f86a3f07b62920694b4626d
flein890 <emifran2812@outlook.com>
2026-04-02 21:29:54 -0300

    Arreglado error de negativo en ej2 y agregada validacion

--- TP1-Recursividad/punto 2/ej2.c
+++ TP1-Recursividad/punto 2/ej2.c
@@ -3,16 +3,36 @@
 int producto(int,int);
 
 int main (void){
-    int num = 3;
-    int num2 = 9;
+    int num;
+    int num2; 
+
+    printf("Ingrese el numero a multiplicar: \n");
+    int num1Valido = scanf("%d",&num);
+    while(num1Valido == 0){
+        printf("Numero invalido, ingrese el numero a multiplicar: \n");
+        while(getchar() == '\n');
+        num1Valido = scanf("%d",&num);
+    }
+
+    printf("Ingrese por cuanto se quiere multiplicar: \n");
+    int num2Valido = scanf("%d",&num2);
+    while(num2Valido == 0){
+        printf("Numero invalido, ingrese el numero a multiplicar: \n");
+        while(getchar() == '\n');
+        num2Valido = scanf("%d",&num2);
+    }
 
     int res = producto(num,num2);
-    printf("%d",res);
+    printf("resultado: %d",res);
 }
 
 int producto(int a,int b){
     if(b == 1){
        return a; 
     }
+    else if(b<0){
+       return -producto(a, -b);
+    }
     return a + producto(a,b-1);
 }
+
