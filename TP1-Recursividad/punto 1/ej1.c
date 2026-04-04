023544d2649c91d1efd98650129711fd97418b1a
flein890 <emifran2812@outlook.com>
2026-04-04 11:33:45 -0300

    validacion ej1

--- TP1-Recursividad/punto 1/ej1.c
+++ TP1-Recursividad/punto 1/ej1.c
@@ -1,16 +1,61 @@
 #include "../Headers/Headers.h"
 
-int main() {
-  
+int main()
+{
+    char cadena[30];
+    bool esValido;
+
+    do{
+        printf("Ingresa una palabra para saber si es un palindromo: ");
+        
+        fgets(cadena,sizeof(cadena),stdin);
+        cadena[strcspn(cadena,"\n")] = '\0';  //Elimino el salto de linea
+
+        esValido=cadenaValida(cadena);  //Validacion de datos
+
+        if(esValido){   
+            printf("Se ingresaron caracteres invalidos, ingrese otra palabra:\n");
+        }
+    } 
+    while(esValido);
+    
+    cadenaMinusculas(cadena);  
+    
+    int tam=strlen(cadena);
+    
+    bool pali=palindromo(cadena,0,tam-1);
+
+    printf("%s",pali ? "Verdadero. La palabra es palindormo." : "Falso. La palabra NO es palindromo.");
+        
+    
+return 0;
 }
+
+void cadenaMinusculas(char *cad)   //Funcion convierte cadena a minusculas
+{
+    for (int i=0; i<strlen(cad); i++){
+        cad[i]=tolower(cad[i]);
+    }
+}
+
+bool cadenaValida(char *cad)      //Funcion para validad 
+{
+    for (int i = 0; i < strlen(cad); i++){
+        if (isalpha(cad[i])){
+            return false;
+        }
+    }
+    return true;
+}
+
   
-bool palindromo(char *cad,int inicio,int fin){}
+bool palindromo(char *cad,int inicio,int fin){
 	   
-    if (inico =>fin){
+    if (inicio >= fin){
         return true;
     }
 
-    if (cad[inicio] != cadena[fin]) {
+    if (cad[inicio] != cad[fin]) {
         return false;
     }
 
